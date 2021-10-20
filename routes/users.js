821635fc14d0ad8fb657d0d304e65f5265fa1879
Roya Chobineh <roya.chbn@gmail.com>
2021-10-20 10:34:03 -0400

    favorit function added

--- routes/users.js
+++ routes/users.js
@@ -21,5 +21,69 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+  router.post("/favorites/:product_id", (req, res) => {
+    const propertyId = req.params.product_id;
+    const userId = 1;
+    const sqlQuery = `INSERT INTO favorite_properties (properties_id, user_id) VALUES ($1, $2)`;
+    const values = [propertyId, userId];
+    db.query(sqlQuery, values)
+      .then((data) => {
+        console.log("data", data);
+        res.redirect("/");
+      })
+      .catch((err) => {
+        console.log("error", err);
+        res.status(500).json({ err: err.message });
+      });
+  });
+  //Rendering the favorite page
+  router.get("/favorites", (req, res) => {
+    const sqlQuery = `SELECT favorite_properties.id AS favorite_id, properties.photo_1, properties.title, properties.price, properties.id AS properties_id, user_id FROM favorite_properties INNER JOIN properties ON properties.id = favorite_properties.properties_id
+    WHERE favorite_properties.user_id= $1;`;
+    let userId = req.session.user_id;
+    const values = [userId];
+    console.log(values);
+    db.query(sqlQuery, values)
+      .then((data) => {
+        const user_email = req.session.user_email;
+        const user_id = req.session.user_id;
+        const templateVars = { favorites: data.rows, user_id, user_email };
+        res.render("favorites", templateVars);
+      })
+      .catch((err) => {
+        res.status(500).json({ err: err.message });
+      });
+  });
+  //Filtering properties by price
+  router.post("/filter", (req, res) => {
+    let sqlQuery = `SELECT properties.id, properties.title as properties_title, properties.price AS properties_price, properties.description as description, properties.photo_1 as properties_photo FROM properties_types JOIN properties ON properties.type_id = properties_types.id`;
+    const min = req.body.minprice;
+    const max = req.body.maxprice;
+    const values = [];
+    if (min && !max) {
+      sqlQuery += ` WHERE properties.price > $1 ORDER BY properties.price;`;
+      values.push(min);
+    } else if (!min && max) {
+      sqlQuery += ` WHERE properties.price < $1 ORDER BY properties.price;`;
+      values.push(max);
+    } else if (!min && !max) {
+      sqlQuery += ` WHERE properties.price > 0 ORDER BY properties.price;`;
+    } else {
+      sqlQuery += ` WHERE properties.price > $1 and properties.price < $2 ORDER BY properties.price;`;
+      values.push(min);
+      values.push(max);
+    }
+    const user_email = req.session.user_email;
+    const userId = req.session.user_id;
+    db.query(sqlQuery, values)
+      .then((data) => {
+        const properties = data.rows;
+        const templateVars = { userId, user_email, properties };
+        res.render("filter", templateVars);
+      })
+      .catch((err) => {
+        res.status(500).json({ err: err.message });
+      });
+  });
   return router;
 };
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-
+const cookieSession = require('cookie-session');
 // PG database client/connection setup
 const { Pool } = require("pg");
 const dbParams = require("./lib/db.js");
@@ -19,7 +19,10 @@ db.connect();
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
-
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2'],
+}));
 app.set("view engine", "ejs");
 // app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -66,8 +69,8 @@ app.post("/login", (req, res) => {
     .then((data) => {
       const user = data.rows[0];
       if (user) {
-        req.body.user_email = user.email;
-        req.body.user_id = user.id;
+        req.session['user_email'] = user.email;
+        req.session['user_id'] = user.id;
         res.redirect("/");
       } else {
         res.send("Unauth access");
