b46d2b7fece5ce6addcd572dbcf919fdac5520ee
Chhavibajajkhurana <89600877+Chhavibajajkhurana@users.noreply.github.com>
2021-10-20 13:19:49 -0400

    user _id is null in server

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get("/views/details/:id", (req, res) => {
   console.log(req.params.id)
   db.query("SELECT * FROM properties WHERE id = $1",[req.params.id]) .then (result => {
     console.log(result.rows)
-    res.render('details',{product:result.rows[0]})
+    res.render('details',{product:result.rows[0], user_id: null})
   })
 
 })
