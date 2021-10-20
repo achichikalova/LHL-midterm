78ff0f61f2e34f19f8b17632cbf5c142dd7a907e
Antonina Chichikalova <tonia.chichikalova@gmail.com>
2021-10-19 21:17:53 -0400

    revert all changes in DB to avoid conflicts

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -5,7 +5,8 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR (255) NOT NULL,
-  phone BIGINT,
+  phone INTEGER,
   city VARCHAR (255),
-  is_admin BOOLEAN DEFAULT FALSE
+  is_admin BOOLEAN DEFAULT FALSE,
+  password INTEGER(255)
 );
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,4 +1,4 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, email, phone, city, is_admin) VALUES ('Roya', 'roya@gmail.com', 2147483647, 'Toronto', True);
-INSERT INTO users (name, email, phone, city, is_admin) VALUES ('Tony', 'tony@gmail.com', 2147453647, 'Montreal', False);
-INSERT INTO users (name, email, phone, city, is_admin) VALUES ('chhavi', 'chhavi@gmail.com', 2147434647, 'Vancouver', False);
+INSERT INTO users (name, email, phone, city, is_admin) VALUES ('Roya', 'roya@gmail.com', 4161111111, 'Toronto', True, 1111);
+INSERT INTO users (name, email, phone, city, is_admin) VALUES ('Tony', 'tony@gmail.com', 4371111111, 'Montreal', False, 1111);
+INSERT INTO users (name, email, phone, city, is_admin) VALUES ('chhavi', 'chhavi@gmail.com', 4671111111, 'Vancouver', False, 1111);
