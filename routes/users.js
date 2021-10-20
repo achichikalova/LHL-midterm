7feabf1200e0e8606176e6cac4f023e3b93b7706
Roya Chobineh <roya.chbn@gmail.com>
2021-10-20 11:56:24 -0400

    removed filter function

--- routes/users.js
+++ routes/users.js
@@ -54,36 +54,5 @@ module.exports = (db) => {
         res.status(500).json({ err: err.message });
       });
   });
-  //Filtering properties by price
-  router.post("/filter", (req, res) => {
-    let sqlQuery = `SELECT properties.id, properties.title as properties_title, properties.price AS properties_price, properties.description as description, properties.photo_1 as properties_photo FROM properties_types JOIN properties ON properties.type_id = properties_types.id`;
-    const min = req.body.minprice;
-    const max = req.body.maxprice;
-    const values = [];
-    if (min && !max) {
-      sqlQuery += ` WHERE properties.price > $1 ORDER BY properties.price;`;
-      values.push(min);
-    } else if (!min && max) {
-      sqlQuery += ` WHERE properties.price < $1 ORDER BY properties.price;`;
-      values.push(max);
-    } else if (!min && !max) {
-      sqlQuery += ` WHERE properties.price > 0 ORDER BY properties.price;`;
-    } else {
-      sqlQuery += ` WHERE properties.price > $1 and properties.price < $2 ORDER BY properties.price;`;
-      values.push(min);
-      values.push(max);
-    }
-    const user_email = req.session.user_email;
-    const userId = req.session.user_id;
-    db.query(sqlQuery, values)
-      .then((data) => {
-        const properties = data.rows;
-        const templateVars = { userId, user_email, properties };
-        res.render("filter", templateVars);
-      })
-      .catch((err) => {
-        res.status(500).json({ err: err.message });
-      });
-  });
   return router;
 };
