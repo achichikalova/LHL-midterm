e2489fd9cbc8d15ab503195c864068ed44bbc169
Roya Chobineh <roya.chbn@gmail.com>
2021-10-20 10:43:23 -0400

    favorite function added

--- routes/users.js
+++ routes/users.js
@@ -21,5 +21,39 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+  //favorites get
+  router.post("/favorites/:product_id", (req, res) => {
+    const propertyId = req.params.product_id;
+    const userId = 1;
+    const sqlQuery = `INSERT INTO favorite_properties (properties_id, user_id) VALUES ($1, $2)`;
+    const values = [propertyId, userId];
+    db.query(sqlQuery, values)
+      .then((data) => {
+        console.log("data", data);
+        res.redirect("/");
+      })
+      .catch((err) => {
+        console.log("error", err);
+        res.status(500).json({ err: err.message });
+      });
+  });
+  //Rendering the favorite page
+  router.get("/favorites", (req, res) => {
+    const sqlQuery = `SELECT favorite_properties.id AS favorite_id, properties.photo_1, properties.title, properties.price, properties.id AS properties_id, user_id FROM favorite_properties INNER JOIN properties ON properties.id = favorite_properties.properties_id
+    WHERE favorite_properties.user_id= $1;`;
+    let userId = req.session.user_id;
+    const values = [userId];
+    console.log(values);
+    db.query(sqlQuery, values)
+      .then((data) => {
+        const user_email = req.session.user_email;
+        const user_id = req.session.user_id;
+        const templateVars = { favorites: data.rows, user_id, user_email };
+        res.render("favorites", templateVars);
+      })
+      .catch((err) => {
+        res.status(500).json({ err: err.message });
+      });
+  });
   return router;
 };
